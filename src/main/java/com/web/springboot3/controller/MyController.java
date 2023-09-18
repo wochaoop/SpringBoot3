80407e835783501c882e34c2fd12557d999501d5
xiwangly2 <1334850101@qq.com>
2023-09-18 21:05:45 +0800

    成功了

--- src/main/java/com/web/springboot3/controller/MyController.java
+++ src/main/java/com/web/springboot3/controller/MyController.java
@@ -2,11 +2,13 @@ package com.web.springboot3.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
+    @ResponseBody
     @GetMapping("/")
     public String rootPath() {
-        return "http://127.0.0.1:8080/";
+        return "这是一个主页";
     }
 }

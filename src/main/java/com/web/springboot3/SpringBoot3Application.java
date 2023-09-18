2ae5386851b79546228440fb3e2e1a6a083f3aec
xiwangly2 <1334850101@qq.com>
2023-09-18 22:20:04 +0800

    小改了一下配置文件

--- src/main/java/com/web/springboot3/SpringBoot3Application.java
+++ src/main/java/com/web/springboot3/SpringBoot3Application.java
@@ -8,6 +8,7 @@ public class SpringBoot3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot3Application.class, args);
+        System.out.println("Server is running at http://127.0.0.1:8080/");
     }
 
 }
--- src/main/java/com/web/springboot3/controller/MyController.java
+++ src/main/java/com/web/springboot3/controller/MyController.java
@@ -1,12 +1,10 @@
 package com.web.springboot3.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
-    @ResponseBody
     @GetMapping("/")
     public String rootPath() {
         return "这是一个主页";
